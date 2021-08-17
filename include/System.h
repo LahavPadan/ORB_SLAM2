8494fa916d2baace67b130b85f0e38f9affc7400
LahavPadan <64014604+LahavPadan@users.noreply.github.com>
2021-08-17 12:42:33 +0300

    Update System.h

--- include/System.h
+++ include/System.h
@@ -114,8 +114,9 @@ public:
 
     // TODO: Save/Load functions
     // SaveMap(const string &filename);
+    Map *GetMap() { return mpMap; };
     // LoadMap(const string &filename);
-
+    
     // Information from most recent processed frame
     // You can call this right after TrackMonocular (or stereo or RGBD)
     int GetTrackingState();
